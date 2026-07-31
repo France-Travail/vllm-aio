18a3c0ad9b4e073fe428f331220d876d45cc6961
maxDavid40 <maxdavid40@gmail.com>
2026-07-31 08:11:06 +0200

    remove vllm install line

--- scripts/setup_venv.sh
+++ scripts/setup_venv.sh
@@ -3,8 +3,7 @@ VLLM_VERSION="$(cat VLLM_VERSION | tr -d '[:space:]')"
 pip install --upgrade pip uv
 
 echo "== Installing vLLM ${VLLM_VERSION} (match the Dockerfile's version!) =="
-uv pip install "vllm==${VLLM_VERSION}"
+uv pip install vllm[audio]==${VLLM_VERSION}
 
 echo "== Installing the custom endpoint plugin in editable mode =="
-uv pip install vllm[audio]==${VLLM_VERSION}
 uv pip install -e .
